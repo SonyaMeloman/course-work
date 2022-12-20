6b417758270d6e6110fc54c95b187d46b6d7a802
SonyaMeloman <110214741+SonyaMeloman@users.noreply.github.com>
2022-12-20 14:46:18 +0400

    Update TestStreamlit.py

--- TestStreamlit.py
+++ TestStreamlit.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib
 from sklearn.preprocessing import MinMaxScaler
+import joblib
 import os
 
 # temsorflow
